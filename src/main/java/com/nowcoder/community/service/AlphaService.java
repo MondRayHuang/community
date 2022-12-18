1d6068d491aae7cb5f7f066b4e3a51c827b9894e
Huangwenrui <2469245827@qq.com>
2022-12-19 01:21:37 +0800

    Initial commit

---
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -0,0 +1,35 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.dao.AlphaDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Service
+//@Scope("prototype")//可以生成多个实例，摆脱单例模式
+public class AlphaService {
+    @Autowired
+    public AlphaDao alphaDao;
+
+    public AlphaService(){
+        System.out.println("实例化AlphaService");
+    }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("初始化AlphaService");
+    }
+
+    @PreDestroy
+    public void destory(){
+        System.out.println("销毁AlphaService");
+    }
+
+    public String find(){
+        return alphaDao.select();
+    }
+
+}
